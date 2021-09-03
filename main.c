ef45258722e87b23c2fa2c9f73081e773ce86a86
chaic <chaic@DESKTOP-R9RR6C5>
2021-09-03 16:40:09 -0600

    Inicio de proyecto 1% Provando leer archivo txt

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc, char** argv) {
     // Creamos un obejto para poder abrir archivo de texto
-    FILE *Datos  = fopen("PeliculasFavoritasGR12043ESD135.txt", "rb");
+    FILE *Datos  = fopen("peliculasFavoritasESD135_2021.csv", "rb");
     //Validamos ejecucion de lectura de archivo.
     if(Datos == NULL){
         perror("Error al abrir archivo."); 
@@ -20,7 +22,7 @@ int main(int argc, char** argv) {
  
     printf("\n\nSe ha leido el archivo.");
     
-    printf("largo %d anchod", Datos->_lbfsize);
+    printf("largo %d anchod");
     
     
     
