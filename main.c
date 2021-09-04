5881dbaff1ee0a2fb9079b186e7af5a25d4e6b26
chaic <chaic@DESKTOP-R9RR6C5>
2021-09-03 19:38:24 -0600

    Inicio de proyecto 2% Provando lea csv (ya con idea de como realizar entradas para structura)

--- main.c
+++ main.c
@@ -2,20 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
 int main(int argc, char** argv) {
     // Creamos un obejto para poder abrir archivo de texto
-    FILE *Datos  = fopen("peliculasFavoritasESD135_2021.csv", "rb");
+    FILE *Datos  = fopen("peliculasFavoritasESD135_2021.csv", "r");
     //Validamos ejecucion de lectura de archivo.
     if(Datos == NULL){
         perror("Error al abrir archivo."); 
         return 1;
-                        
+        
     }
-    char caracter;
-    while(feof(Datos)==0){
-         caracter = fgetc(Datos);
-         printf("%c", caracter);
-         }
+    char caracter[200];
+    while(fgets(caracter, sizeof(caracter), Datos)){
+        char *token;
+        token = strtok(caracter, "\n");
+        while(token!=NULL){
+         printf("%s", token);
+         token =strtok(NULL, "\n");
+        }
+        printf("\n");
+    }
     fclose(Datos);
  
     printf("\n\nSe ha leido el archivo.");
