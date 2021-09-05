5cc03bc8db584ece7146adfbb525a4c0fadd721d
chaic <chaic@DESKTOP-R9RR6C5>
2021-09-04 23:59:47 -0600

    Rellenando Matriz Invertida

--- main.c
+++ main.c
@@ -14,15 +14,7 @@ int main(int argc, char** argv) {
     //Validamos ejecucion de lectura de archivo.
     if (Datos == NULL) {
         perror("Error al abrir archivo.");
-        return 1;
-        
-        
-        
-        
-        
-        
-        printf("");
-
+        return 1;         
     }
     int coma = 0, largo=0, ancho=0, aux;
     char caracter[59];
@@ -33,45 +25,45 @@ int main(int argc, char** argv) {
         
          if (coma > 1) {
             while (token != NULL) {
-                   switch (ancho){
+                   
+                switch (ancho){
                        
                        case 1:
                         aux =  strtol(token, NULL, 10);
-                        Peliculas[ancho][largo].TheGodfather = aux;
-                        printf("guardo exitoso el valor %i", Peliculas[ancho][largo].TheGodfather);
+                        Peliculas[ancho-1][largo].TheGodfather = aux;
+                        printf("guardo exitoso el valor %i", Peliculas[ancho-1][largo].TheGodfather);
                         break;
                     case 2:
                         aux = (int) strtol(token, NULL, 10);
-                        Peliculas[ancho][largo].TokyoDrift = aux;
-                        printf("guardo exitoso el valor %d", Peliculas[ancho][largo].TokyoDrift);
+                        Peliculas[ancho-1][largo].TokyoDrift = aux;
+                        printf("guardo exitoso el valor %d", Peliculas[ancho-1][largo].TokyoDrift);
                         break;
                     case 3:
                         aux = (int) strtol(token, NULL, 10);
-                        Peliculas[ancho][largo].SpiritedAway = aux;
-                        printf("guardo exitoso el valor %d", Peliculas[ancho][largo].SpiritedAway);
+                        Peliculas[ancho-1][largo].SpiritedAway = aux;
+                        printf("guardo exitoso el valor %d", Peliculas[ancho-1][largo].SpiritedAway);
                         break;
                     case 4:                        
                         aux = (int) strtol(token, NULL, 10);
-                        Peliculas[ancho][largo].Tenet = aux;
-                        printf("guardo exitoso el valor %d", Peliculas[ancho][largo].Tenet);
+                        Peliculas[ancho-1][largo].Tenet = aux;
+                        printf("guardo exitoso el valor %d", Peliculas[ancho-1][largo].Tenet);
                         break;
                     case 5: 
                         aux = (int) strtol(token, NULL, 10);
-                        Peliculas[ancho][largo].TheGrandBudapestHotel = aux;
-                        printf("guardo exitoso el valor %d", Peliculas[ancho][largo].TheGrandBudapestHotel);
+                        Peliculas[ancho-1][largo].TheGrandBudapestHotel = aux;
+                        printf("guardo exitoso el valor %d", Peliculas[ancho-1][largo].TheGrandBudapestHotel);
                         break;
                     case 6:
                         aux = (int) strtol(token, NULL, 10);
-                        Peliculas[ancho][largo].Frozen = aux;
-                        printf("guardo exitoso el valor %d", Peliculas[ancho][largo].TheGodfather);
+                        Peliculas[ancho-1][largo].Frozen = aux;
+                        printf("guardo exitoso el valor %d", Peliculas[ancho-1][largo].TheGodfather);
                         break;
                     default:
                         break;                        
                 }
+                   ancho++;
                    printf("\n");
-                    token = strtok(NULL, ",");
-                
-                    ancho++;
+                    token = strtok(NULL, ",");                                   
                 
             }
             printf("\n");
@@ -94,38 +86,39 @@ int main(int argc, char** argv) {
 }
 
 void LlenarInversionDePeliculas(){
+    printf("valor de 1 %d \n", Peliculas[0][0].TheGodfather);
+    printf("valor de 1 %d", Peliculas[1][0].TheGodfather);
+    printf("valor de 1 %d\n", Peliculas[3][0].TheGodfather);
     for(int largo=0;largo<=58;largo++){
         for(int ancho=0;ancho<=5;ancho++){
-            switch (ancho){                       
-                       case 0:
-                        InversionDePeliculas[largo][ancho].TheGodfather=Peliculas[ancho][largo].TheGodfather;
-                        printf("p1r %d", InversionDePeliculas[ancho][largo].TheGodfather);
-                        break;
-                    case 1:
-                        InversionDePeliculas[largo][ancho].TokyoDrift=Peliculas[ancho][largo].TokyoDrift;
-                        printf("p2 %d", InversionDePeliculas[ancho][largo].TokyoDrift);
-                        break;
-                    case 2:
-                        InversionDePeliculas[largo][ancho].SpiritedAway=Peliculas[ancho][largo].SpiritedAway;
-                        printf("p3 %d", InversionDePeliculas[ancho][largo].SpiritedAway);
-                        break;
-                    case 3:                        
-                        InversionDePeliculas[largo][ancho].Tenet=Peliculas[ancho][largo].Tenet;
-                        printf("p4 %d", InversionDePeliculas[ancho][largo].Tenet);
-                        break;
-                    case 4: 
-                        InversionDePeliculas[largo][ancho].TheGrandBudapestHotel=Peliculas[ancho][largo].TheGrandBudapestHotel;
-                        printf("p5 %d", InversionDePeliculas[ancho][largo].TheGrandBudapestHotel);
-                        break;
-                    case 5:
-                       InversionDePeliculas[largo][ancho].Frozen=Peliculas[ancho][largo].Frozen;
-                        printf("p6 %d", InversionDePeliculas[ancho][largo].Frozen);
-                        break;
-                    default:
-                        break;                        
-                }
-            printf("\t");
-        }    
-        printf("\n \n");
-    }
+            switch (ancho){
+                case 0:
+                    InversionDePeliculas[largo][ancho].TheGodfather=Peliculas[ancho][largo].TheGodfather;
+                printf("%d", InversionDePeliculas[largo][ancho].TheGodfather);
+                break;
+                case 1:
+                    InversionDePeliculas[largo][ancho].TokyoDrift=Peliculas[ancho][largo].TokyoDrift;
+            printf("%d", InversionDePeliculas[largo][ancho].TokyoDrift);
+            break;
+                case 2:
+                    InversionDePeliculas[largo][ancho].SpiritedAway=Peliculas[ancho][largo].SpiritedAway;
+            printf("%d", InversionDePeliculas[largo][ancho].SpiritedAway);
+            break;
+                case 3:
+                    InversionDePeliculas[largo][ancho].Tenet=Peliculas[ancho][largo].Tenet;
+            printf("%d", InversionDePeliculas[largo][ancho].Tenet);
+            break;
+                case 4:
+                    InversionDePeliculas[largo][ancho].TheGrandBudapestHotel=Peliculas[ancho][largo].TheGrandBudapestHotel;
+            printf("%d", InversionDePeliculas[largo][ancho].TheGrandBudapestHotel);
+            break;
+                case 5:
+                    InversionDePeliculas[largo][ancho].Frozen=Peliculas[ancho][largo].Frozen;
+            printf("%d", Peliculas[largo][ancho].Frozen);
+            break;
+                default:
+                    break;
+            }}
+        printf("\n");
+        }
 }
