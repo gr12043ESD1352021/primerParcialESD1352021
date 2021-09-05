9532889c81004ec87a453d22f6e275b6b2827c06
chaic <chaic@DESKTOP-R9RR6C5>
2021-09-04 21:43:01 -0600

    Introducioendo valores a vector

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- struct Pelicua{
+ struct Pelicula{
      
     int TheGodfather, TokyoDrift, SpiritedAway, Tenet, TheGrandBudapestHotel, Frozen;
     
-}Peliculas[5][58];
+ }Peliculas[5][58], InversionDePeliculas[58][5], MultiplicandoPeliculaPorInversion[58][58];
 
 int main(int argc, char** argv) {
     // Creamos un obejto para poder abrir archivo de texto
@@ -15,6 +15,13 @@ int main(int argc, char** argv) {
     if (Datos == NULL) {
         perror("Error al abrir archivo.");
         return 1;
+        
+        
+        
+        
+        
+        
+        printf("");
 
     }
     int coma = 0, largo=0, ancho=0, aux;
@@ -81,6 +88,44 @@ int main(int argc, char** argv) {
     }
     fclose(Datos);
 
-    printf("\n\nSe ha leido el archivo.");
+    printf("\n\nSe ha leido el archivo.\n");
+    LlenarInversionDePeliculas();
     return 0;
+}
+
+void LlenarInversionDePeliculas(){
+    for(int largo=0;largo<=58;largo++){
+        for(int ancho=0;ancho<=5;ancho++){
+            switch (ancho){                       
+                       case 0:
+                        InversionDePeliculas[largo][ancho].TheGodfather=Peliculas[ancho][largo].TheGodfather;
+                        printf("p1r %d", InversionDePeliculas[ancho][largo].TheGodfather);
+                        break;
+                    case 1:
+                        InversionDePeliculas[largo][ancho].TokyoDrift=Peliculas[ancho][largo].TokyoDrift;
+                        printf("p2 %d", InversionDePeliculas[ancho][largo].TokyoDrift);
+                        break;
+                    case 2:
+                        InversionDePeliculas[largo][ancho].SpiritedAway=Peliculas[ancho][largo].SpiritedAway;
+                        printf("p3 %d", InversionDePeliculas[ancho][largo].SpiritedAway);
+                        break;
+                    case 3:                        
+                        InversionDePeliculas[largo][ancho].Tenet=Peliculas[ancho][largo].Tenet;
+                        printf("p4 %d", InversionDePeliculas[ancho][largo].Tenet);
+                        break;
+                    case 4: 
+                        InversionDePeliculas[largo][ancho].TheGrandBudapestHotel=Peliculas[ancho][largo].TheGrandBudapestHotel;
+                        printf("p5 %d", InversionDePeliculas[ancho][largo].TheGrandBudapestHotel);
+                        break;
+                    case 5:
+                       InversionDePeliculas[largo][ancho].Frozen=Peliculas[ancho][largo].Frozen;
+                        printf("p6 %d", InversionDePeliculas[ancho][largo].Frozen);
+                        break;
+                    default:
+                        break;                        
+                }
+            printf("\t");
+        }    
+        printf("\n \n");
+    }
 }
