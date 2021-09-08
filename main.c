03bfc172c8fbc80e46bc66d5ba86eea7b0bf0fca
chaic <chaic@DESKTOP-R9RR6C5>
2021-09-07 22:33:46 -0600

    Final 2.0

--- main.c
+++ main.c
@@ -5,15 +5,12 @@
 struct Pelicula {
     /*arreglando matriz*/
     int Matriz[6][59];
-    int MatrizInversa[59][6];
-    int MultiploDeMatrices[59][59];
 } Peliculas;
 
 int main(int argc, char** argv) {
     Integrantes();
-    // Creamos un obejto para poder abrir archivo de texto
+    // Creamos un obejto para poder abrir archivo de texto.
     FILE *Datos = fopen("peliculasFavoritasESD135_2021.csv", "r");
-    
     //Validamos ejecucion de lectura de archivo.
     if (Datos == NULL) {
         perror("Error al abrir archivo.");
@@ -22,13 +19,12 @@ int main(int argc, char** argv) {
     LlenandoMatriz(*Datos);
     LlenandoInversa();
     printf("\n\n");
-    MultiplicandoMatriz();
     fclose(Datos);
     return 0;
 }
 
-void LlenandoMatriz(FILE *Datos){
-
+void LlenandoMatriz(FILE *Datos) {
+    //Iniciamos a llenar matriz con los datos del CSV.
     int coma = 0, largo = 0, ancho = 0, aux;
     char caracter[60];
     char *token;
@@ -65,37 +61,90 @@ void LlenandoMatriz(FILE *Datos){
         printf("\n");
     }
     printf("\n\n");
+    free(token);
 }
 
 void LlenandoInversa() {
+    int **MatrizInversa;
+    MatrizInversa = (int**) malloc(58 * sizeof (int*));
+    if (MatrizInversa == NULL) {
+        printf("Error al reservar meroria.");
+        return 1;
+    }
+    for (int largo = 0; largo < 59; largo++) {
+        for (int ancho = 0; ancho < 6; ancho++) {
+            MatrizInversa[largo] = (int*) malloc(ancho * sizeof (int));
+            if (MatrizInversa[largo] == NULL) {
+                printf("Error al reservar meroria.");
+                return 1;
+            }
+        }
+    }
     for (int a = 0; a < 59; a++) {
         for (int b = 0; b < 6; b++) {
-            Peliculas.MatrizInversa[a][b] = Peliculas.Matriz[b][a];
+            MatrizInversa[a][b] = Peliculas.Matriz[b][a];
         }
     }
     for (int b = 0; b < 6; b++) {
         for (int a = 0; a < 59; a++) {
             printf("%i ", Peliculas.Matriz[b][a]);
-
         }
         printf("\n");
     }
+    printf("\n\n");
+    MultiplicandoMatriz(MatrizInversa);
+    //Liberamos memoria de puntero.
+    for (int b = 0; b < 59; b++) {
+        for (int a = 0; a < 6; a++) {
+            free(MatrizInversa[a][b]);
+        }
+    }
 }
 
-void MultiplicandoMatriz() {
+void MultiplicandoMatriz(int **MatrizInversa) {
+    int **MultiploDeMatrices;
+    MultiploDeMatrices = (int**) malloc(58 * sizeof (int*));
+    if (MultiploDeMatrices == NULL) {
+        printf("Error al reservar meroria.");
+        return 1;
+    }
+    for (int largo = 0; largo < 59; largo++) {
+
+        for (int ancho = 0; ancho < 59; ancho++) {
+            MultiploDeMatrices[largo] = (int*) malloc(ancho * sizeof (int));
+            if (MultiploDeMatrices[largo] == NULL) {
+                printf("Error al reservar meroria.");
+                return 1;
+            }
+        }
+    }
     for (int largoMult = 0; largoMult < 59; largoMult++) {
         for (int largo = 0; largo < 59; largo++) {
             for (int ancho = 0; ancho < 6; ancho++) {
-                Peliculas.MultiploDeMatrices[largoMult][largo] += Peliculas.Matriz[ancho][largoMult] * Peliculas.MatrizInversa[largo][ancho];
+                MultiploDeMatrices[largoMult][largo] += Peliculas.Matriz[ancho][largoMult] * MatrizInversa[largo][ancho];
             }
         }
     }
     for (int b = 0; b < 59; b++) {
         for (int a = 0; a < 59; a++) {
-            if(a==b){printf("n  ");}
-            else{printf("%i  ", Peliculas.MultiploDeMatrices[b][a]);}
+            if (a == b) {
+                printf("NL ");
+            } else {
+                printf("%i  ", MultiploDeMatrices[b][a]);
+            }
         }
         printf("\n");
+    }
+    //Liberamos memoria de los punteros.
+    for (int b = 0; b < 59; b++) {
+        for (int a = 0; a < 6; a++) {
+            free(MatrizInversa[a][b]);
+        }
+    }
+    for (int b = 0; b < 59; b++) {
+        for (int a = 0; a < 59; a++) {
+            free(MultiploDeMatrices[a][b]);
+        }
     }
 }
 
